e9b79e5d8bda2dc4e1d080828daa1a61123fd867
EgorAlmikeev <ealmikeew@gmail.com>
2018-10-09 23:42:31 +0300

    Add files via upload

---
+++ self/report_1_2/guest_book/__init__.py
@@ -0,0 +1,18 @@
+import inspect
+import os
+import sys
+
+def get_script_dir(follow_symlinks=True):
+    if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+
+sys.path.append(get_script_dir())
+
+from guest_book_classes import JSONReadWritter
+from guest_book_classes import GuestBook
+from guest_book_classes import Guest
