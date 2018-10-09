b0d4fcc21321908fe8546de6cbf6029e45fa33a8
EgorAlmikeev <ealmikeew@gmail.com>
2018-10-09 23:43:33 +0300

    Update __init__.py

--- self/report_1_2/guest_book/__init__.py
+++ self/report_1_2/guest_book/__init__.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 def get_script_dir(follow_symlinks=True):
+    """
+    https://stackoverflow.com/questions/3718657/how-to-properly-determine-current-script-directory/22881871#22881871
+    """
     if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
         path = os.path.abspath(sys.executable)
     else:
